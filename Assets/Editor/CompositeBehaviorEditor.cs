4037683bb828a2c9409466ba419157acfcaf36e3
Roberto Serra <obi.serra@gmail.com>
2023-05-05 14:13:30 +0200

    setup in project

--- Assets/Editor/CompositeBehaviorEditor.cs
+++ Assets/Editor/CompositeBehaviorEditor.cs
@@ -14,53 +14,59 @@ public class CompositeBehaviorEditor : Editor
         Rect r = EditorGUILayout.BeginHorizontal();
         r.height = EditorGUIUtility.singleLineHeight;
 
-        //check for behaviors
-        if (cb.behaviors == null || cb.behaviors.Length == 0)
+               if (GUI.Button(r, "Poppa"))
         {
-            EditorGUILayout.HelpBox("No behaviors in array.", MessageType.Warning);
-            EditorGUILayout.EndHorizontal();
-            r = EditorGUILayout.BeginHorizontal();
-            r.height = EditorGUIUtility.singleLineHeight;
+            Debug.Log("Poppa");
         }
-        else
-        {
-            r.x = 30f;
-            r.width = EditorGUIUtility.currentViewWidth - 95f;
-            EditorGUI.LabelField(r, "Behaviors");
-            r.x = EditorGUIUtility.currentViewWidth - 65f;
-            r.width = 60f;
-            EditorGUI.LabelField(r, "Weights");
-            r.y += EditorGUIUtility.singleLineHeight * 1.2f;
 
-            EditorGUI.BeginChangeCheck();
-            for (int i = 0; i < cb.behaviors.Length; i++)
-            {
-                r.x = 5f;
-                r.width = 20f;
-                EditorGUI.LabelField(r, i.ToString());
-                r.x = 30f;
-                r.width = EditorGUIUtility.currentViewWidth - 95f;
-                cb.behaviors[i] = (FlockBehavior)EditorGUI.ObjectField(r, cb.behaviors[i], typeof(FlockBehavior), false);
-                r.x = EditorGUIUtility.currentViewWidth - 65f;
-                r.width = 60f;
-                cb.weights[i] = EditorGUI.FloatField(r, cb.weights[i]);
-                r.y += EditorGUIUtility.singleLineHeight * 1.1f;
-            }
-            if (EditorGUI.EndChangeCheck())
-            {
-                EditorUtility.SetDirty(cb);
-            }
-        }
+        //check for behaviors
+        // if (cb.behaviors == null || cb.behaviors.Length == 0)
+        // {
+        //     EditorGUILayout.HelpBox("No behaviors in array.", MessageType.Warning);
+        //     EditorGUILayout.EndHorizontal();
+        //     r = EditorGUILayout.BeginHorizontal();
+        //     r.height = EditorGUIUtility.singleLineHeight;
+        // }
+        // else
+        // {
+        //     r.x = 30f;
+        //     r.width = EditorGUIUtility.currentViewWidth - 95f;
+        //     EditorGUI.LabelField(r, "Behaviors");
+        //     r.x = EditorGUIUtility.currentViewWidth - 65f;
+        //     r.width = 60f;
+        //     EditorGUI.LabelField(r, "Weights");
+        //     r.y += EditorGUIUtility.singleLineHeight * 1.2f;
+
+        //     EditorGUI.BeginChangeCheck();
+        //     for (int i = 0; i < cb.behaviors.Length; i++)
+        //     {
+        //         r.x = 5f;
+        //         r.width = 20f;
+        //         EditorGUI.LabelField(r, i.ToString());
+        //         r.x = 30f;
+        //         r.width = EditorGUIUtility.currentViewWidth - 95f;
+        //         cb.behaviors[i] = (FlockBehavior)EditorGUI.ObjectField(r, cb.behaviors[i], typeof(FlockBehavior), false);
+        //         r.x = EditorGUIUtility.currentViewWidth - 65f;
+        //         r.width = 60f;
+        //         cb.weights[i] = EditorGUI.FloatField(r, cb.weights[i]);
+        //         r.y += EditorGUIUtility.singleLineHeight * 1.1f;
+        //     }
+        //     if (EditorGUI.EndChangeCheck())
+        //     {
+        //         EditorUtility.SetDirty(cb);
+        //     }
+        // }
 
         EditorGUILayout.EndHorizontal();
         r.x = 5f;
         r.width = EditorGUIUtility.currentViewWidth - 10f;
-        r.y += EditorGUIUtility.singleLineHeight * 0.5f;
+        r.y += EditorGUIUtility.singleLineHeight * 2.5f;
         if (GUI.Button(r, "Add Behavior"))
         {
             AddBehavior(cb);
             EditorUtility.SetDirty(cb);
         }
+    
 
         r.y += EditorGUIUtility.singleLineHeight * 1.5f;
         if (cb.behaviors != null && cb.behaviors.Length > 0)
